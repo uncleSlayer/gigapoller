0aaa2919ef1e071e6adc9930d294b12ed4b6f735
siddhant ota <otasiddhant@gmail.com>
2023-01-25 23:23:07 +0530

    signup added

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -4,7 +4,7 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/signup', methods = ['GET', 'POST'])
 def signup():
-    from firebase_admin.auth import get_user_by_email
+    from firebase_admin.auth import get_user_by_email, create_user
     from app import fb_app
 
 
@@ -16,12 +16,28 @@ def signup():
 
         print(email, password_one, password_two)
 
+        user = None
+
         if password_one != password_two:
             flash('Passwords do not match')
+
+        if len(password_one) < 6:
+            flash('Password must be minimum of 6 characters')
         
         else:
-            user = get_user_by_email(email, fb_app)
-            print(user)
+
+            try:
+                user = get_user_by_email(email, fb_app)
+
+                flash('User with this email already exists.')
+
+            except Exception as e:
+
+                print(e)
+                create_user(email= email, password = password_one)
+                the_user = get_user_by_email(email)
+                print(the_user)
+            
 
 
     return render_template('signup.html')
